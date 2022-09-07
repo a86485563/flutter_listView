6fc76e00d7dd57df679cc579a12a19f85cf6d0c4
a86485563 <a86485563@gmail.com>
2022-09-07 14:59:35 +0800

    增加愛心圖示。

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,55 +10,62 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    
     return MaterialApp(
       title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
-           title: const Text('Startup Name Generator'),
+          title: const Text('Startup Name Generator'),
         ),
-        body: const Center(
-          child: RandomWords()
-        ),
+        body: const Center(child: RandomWords()),
       ),
     );
   }
 }
 
-class RandomWords extends StatefulWidget{
+class RandomWords extends StatefulWidget {
   const RandomWords({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
-      return _RandomWordsState();
+    return RandomWordsState();
   }
 }
 
-class _RandomWordsState extends State<RandomWords>{
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+//用set來存喜歡的字，因為se不能重複list可以。
+  final Set<WordPair> _saved = new Set<WordPair>();
 
-final _suggestions = <WordPair>[];
-final _biggerFont = const TextStyle(fontSize: 18);
-
- @override
-    Widget build(BuildContext context) {
-
-      return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return const Divider(); /*2*/
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-            }
-          return ListTile(
-            title: Text( 
-              _suggestions[index].asPascalCase,
-              style: _biggerFont
-              ),
-          );
-          },
-      );
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return const Divider(); /*2*/
+        final index = i ~/ 2; /*3*/ //i~/2 代表i除以2取整數的部分。
+        if (index >= _suggestions.length) {
+          //當index 大於要取的值時，就在call api 取10個放進list裡頭
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
   }
 
+//用此method 建立一行字、一顆心得row
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+    );
+  }
 }
